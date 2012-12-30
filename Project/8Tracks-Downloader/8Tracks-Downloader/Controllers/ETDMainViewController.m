938bb9836a2ff5c29c757cef574f18497190231e
Jose Miguel Salcido Aguilar <jose@otfusion.org>
2012-12-30 06:29:38 -0700

    added: TODO; modified: UI and mainController
    adding TODO;
    modified: UI (lets back to the 100px NSImageView), mainController to
    load 100px not 200px, and some comments.

--- Project/8Tracks-Downloader/8Tracks-Downloader/Controllers/ETDMainViewController.m
+++ Project/8Tracks-Downloader/8Tracks-Downloader/Controllers/ETDMainViewController.m
@@ -20,12 +20,11 @@ NSString * const kActionObtainSongData = @"osd";
 NSString * const kJSONKeyForPlayToken = @"play_token";
 NSString * const kJSONKeyForMix = @"mix";
 NSString * const kJSONKeyForCoverURL = @"cover_urls";
-NSString * const kJSONKeyForCoverURLPx = @"max200";
+NSString * const kJSONKeyForCoverURLPx = @"sq100";
 NSString * const kJSONKeyForDescription = @"description";
 NSString * const kJSONKeyForName = @"name";
 NSString * const kJSONKeyForUser = @"user";
 NSString * const kJSONKeyForUserName = @"slug";
-
 
 @implementation ETDMainViewController
 
@@ -67,6 +66,7 @@ NSString * const kJSONKeyForUserName = @"slug";
     if (self) {
         // Initialization code here.
         actionToPerform = @"";
+        // This must hide the progress indicator, not working, it is weird.
         [self.playlistProgressIndicator setHidden:YES];
     }
     
@@ -75,18 +75,26 @@ NSString * const kJSONKeyForUserName = @"slug";
 
 - (IBAction)lookUpAction:(id)sender
 {
-    // Some business rules here please
+    // Some more business rules here please
     if([[self.devAPIKeyTextField stringValue] isEmpty] ||
         [[self.playlistURLTextField stringValue] isEmpty]) {
-        // NOTIFY THE USER.
-        NSString *msg = @"There can not be empty fields.[end]Playlist URL or Dev API Key fields are empty";
+        
+        // Notify the user
+        // It is a rule that every message shall contain a "Title" and a "Description"
+        // We shall send a single NSString, the Title is before the tag [end]
+        // description is after the tag.
+        NSString *msg = @"There cannot be empty fields.[end]Playlist URL or Dev API Key fields are empty";
         [self alertUserWithMsg:msg];
-        NSLog(@"Some field are empty");
         return;
     } else {
+        // Look Up Button shall be disabled (only 1 request per click)
         [self.lookUpButton setEnabled:NO];
+        
+        // Show the progress indicator and start the animation
         [self.playlistProgressIndicator setHidden:NO];
         [self.playlistProgressIndicator startAnimation:nil];
+        
+        // Start loading data.
         [self lookUp];
     }
 }
@@ -99,29 +107,49 @@ NSString * const kJSONKeyForUserName = @"slug";
 
 -(void)lookUp
 {
+    // Load the play token first (we don't need more data than the developer API key)
     [self startLoadingPlayTokenWithDevAPIKey:[devAPIKeyTextField stringValue]];
 }
 
 -(void)alertUserWithMsg:(NSString *)msg
 {
+    // NSArray that contains the "Title" and the "Description"
     NSArray *msgArray = [msg componentsSeparatedByString:@"[end]"];
+    
+    // Alloc memory for our NSAlert (not MODAL, is a SHEET-MODAL DIALOG)
     NSAlert *alertView = [[NSAlert alloc] init];
+    
+    // Set all the dialog properties
     [alertView addButtonWithTitle:@"OK"];
-    [alertView setMessageText:[msgArray objectAtIndex:0]];
+    [alertView setMessageText:[msgArray objectAtIndex:0]]; // this is why title is FIRST
     [alertView setAlertStyle:NSWarningAlertStyle];
-    [alertView setInformativeText:[msgArray objectAtIndex:1]];
+    [alertView setInformativeText:[msgArray objectAtIndex:1]]; // this is why description is SECOND
+    
+    // This is to call the dialog as a SHEET MODAL DIALOG, not a MODAL ONE (ugly)
     [alertView beginSheetModalForWindow:self.window modalDelegate:self didEndSelector:nil contextInfo:nil];
     
 }
 
 -(void)updateUI:(id)object
 {
+    // Convert the object to the desired type
+    // jmsalcido: I was thinking bout using a "model" to follow the MVC pattern
+    // jmsalcido: but it is... working.
     NSDictionary *dict = object;
+    
+    // Obtain the cool properties (using constants always)
     self.playlistNameTextField.stringValue = [dict valueForKey:kJSONKeyForName];
     self.playlistDescriptionTextField.stringValue = [dict valueForKey:kJSONKeyForDescription];
     self.playlistAuthorTextField.stringValue = [[dict valueForKey:kJSONKeyForUser] valueForKey:kJSONKeyForUserName];
+    
+    // Create the NSURL with the thumbImageURL
     NSURL *URLImage = [[NSURL alloc] initWithString:[[dict valueForKey:kJSONKeyForCoverURL] valueForKey:kJSONKeyForCoverURLPx]];
+    
+    // Retrieve the Image from the interkatz
     [self startLoadingDataWithURL:URLImage action:kActionObtainImageData];
+    
+    // Stop animation for the Progress Indicator and hide it, also
+    // enable the Look Up Button again!
     [self.playlistProgressIndicator stopAnimation:nil];
     [self.playlistProgressIndicator setHidden:YES];
     [self.lookUpButton setEnabled:YES];
@@ -164,7 +192,7 @@ NSString * const kJSONKeyForUserName = @"slug";
     // Create the request.
     NSURLRequest *theRequest=[NSURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:timeOut];
     
-    // create the connection with the request
+    // Create the connection with the request
     // and start loading the data
     NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
     
@@ -177,6 +205,19 @@ NSString * const kJSONKeyForUserName = @"slug";
     }
 }
 
+// --------------------------------------------------
+// GENERIC METHODS startLoadingXXX:
+//    Always calling the master race method:
+//      -(void)startLoadingDataWithURL:action:
+//
+//    Because of... the SUN.
+//    jmsalcido: Actually, because I did not know
+//        that I could use different NSURLConnection
+//        and compare them at didFinishLoadingData...
+//
+//        But, this approach works fine too!
+//
+// --------------------------------------------------
 -(void)startLoadingPlayTokenWithDevAPIKey:(NSString *)devAPIKey
 {
     NSString *urlString = [NSString stringWithFormat:@"http://8tracks.com/sets/new.json?api_key=%@",devAPIKey];
@@ -211,7 +252,7 @@ NSString * const kJSONKeyForUserName = @"slug";
 // --------------------------------------------------
 //  PROTOCOL NSConnectionDelegate METHODS
 // --------------------------------------------------
-
+// PSSST: Copypasted from Apple Developers.
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
 {
     // This method is called when the server has determined that it
@@ -235,43 +276,72 @@ NSString * const kJSONKeyForUserName = @"slug";
           [[error userInfo] objectForKey:NSURLErrorFailingURLStringErrorKey]);
 }
 
+// This method is not copypasted
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
-    // do something with the data
+    // Welp, this line is copypasted.
     NSLog(@"Succeeded! Received %ld bytes of data",[receivedData length]);
     
+    // Transform NSData to NSString
     NSString *dataString = [[NSString alloc] initWithData:receivedData encoding:NSUTF8StringEncoding];
     
     if([actionToPerform isEqualToString:kActionObtainPlayToken]) {
         
+        #pragma mark Did Obtain Play Token
+        // --------------------------------------------------
+        
+        // Using Framework YAJL get a NSDictionary and get the value
         playToken = [[receivedData yajl_JSON] valueForKey:kJSONKeyForPlayToken];
+        
+        // Also, startLoadingHTMLData now.
         [self startLoadingHTMLDataWithURL:[playlistURLTextField stringValue]];
         
     } else if([actionToPerform isEqualToString:kActionObtainHTMLData]) {
         
+        #pragma mark Did Obtain HTML Data
+        // --------------------------------------------------
+        
+        // Obtain playlistId from HTML.
         HTMLData = dataString;
         playlistId = [self obtainPlaylistIdWithHTMLString:dataString];
         
+        // Also, startLoadingPlaylistData
         [self startLoadingPlaylistDataWithId:playlistId devAPIKey:[devAPIKeyTextField stringValue]];
         
     } else if([actionToPerform isEqualToString:kActionObtainPlaylistData]) {
         
-        NSDictionary *mainDictionary = [receivedData yajl_JSON];
-        NSDictionary *mix = [mainDictionary valueForKey:kJSONKeyForMix];
+        #pragma mark Did Obtain Playlist Data
+        // --------------------------------------------------
+        
+        // I do not remember if we shall use other info, but lets comment this line.
+        //NSDictionary *mainDictionary = [receivedData yajl_JSON];
+        
+        // Obtain the mix dictionary
+        NSDictionary *mix = [[receivedData yajl_JSON] valueForKey:kJSONKeyForMix];
+        
+        // Update the UI
         [self updateUI:mix];
         
     } else if([actionToPerform isEqualToString:kActionObtainImageData]) {
         
+        #pragma mark Did Obtain Image Data
+        // --------------------------------------------------
+        
+        // Update NSImageView
         NSImage *image = [[NSImage alloc] initWithData:receivedData];
         [self.playlistImageView setImage:image];
         
     } else if([actionToPerform isEqualToString:kActionObtainSongData]) {
         
+        #pragma mark Did Obtain Song Data
+        // --------------------------------------------------
+        
+        // TODO
         NSLog(@"%@",[receivedData yajl_JSON]);
         
     } else {
         
-        // At least lets see what did we receive.
+        // At least it shall print what did NSURLConnection receive.
         NSLog(@"%@", dataString);
         
     }
